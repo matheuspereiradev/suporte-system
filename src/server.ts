50223df17cbd271ad6d91b63c7f869041d7b0c5f
matheus <matheuslima20111997@gmail.com>
2021-05-27 18:39:19 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,5 @@
+import {app} from '@shared/infra/http/app';
+
+app.listen(3030,()=>{
+    console.log("Running");
+})
---
+++ src/shared/infra/http/app.ts
@@ -0,0 +1,42 @@
+import express, { NextFunction,Response,Request } from 'express';
+import 'express-async-errors'
+import cors from 'cors';
+import CreateConnection from '@shared/infra/typeorm/index';
+import { routes } from '@shared/infra/http/routes/index.routes';
+import 'reflect-metadata';
+import {errors} from 'celebrate'
+import path from 'path';
+
+//import '@shared/container'
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+CreateConnection();
+
+app.use('/files',express.static(path.resolve(__dirname,"..","temp")));
+
+app.use(routes)
+
+app.use(errors())
+/*app.use(
+    (err:Error, request:Request, response:Response, next:NextFunction)=>{
+
+    if(err instanceof Erro){
+        return response.status(err.statusCode).json({
+            "status":"error: "+err.internalCode,
+            "error":err.message
+        })
+    };
+
+    console.error(err);
+
+    return response.status(500).json({
+        "status":"error: 1000",
+        "error":"internal server error"
+    });
+
+})*/
+
+export {app}
