98b1ee9fa7b31ea28ea7dea3dfa200cf3abb1cea
matheus <matheuslima20111997@gmail.com>
2021-05-28 11:38:00 -0300

    rota

--- src/modules/user/controllers/UserController.ts
+++ src/modules/user/controllers/UserController.ts
@@ -1,14 +1,15 @@
 import { Request, Response } from 'express';
 import Erro from '@shared/errors/AppError';
+import { UserRepository } from '@modules/user/infra/typeorm/repositories/UserRepository';
 
 class UserController {
 
     async show(request: Request, response: Response) {
 
-        //const userRepository = new UserRepository();
-        //const all = await userRepository.findAll();
+        const userRepository = new UserRepository();
+        const all = await userRepository.findAll();
 
-        return response.status(200).json({});
+        return response.status(200).json(all);
     }
     
 };
--- src/shared/infra/http/routes/index.routes.ts
+++ src/shared/infra/http/routes/index.routes.ts
@@ -1,7 +1,9 @@
 import {Router,Request,Response} from 'express';
+import { routesUser } from '@modules/user/infra/http/routes/user.routes';
 
 const routes = Router();
 
 routes.get('/healthz',(req:Request,res:Response)=>{res.json({"status":"running"})});
+routes.use('/user',routesUser);
 
 export {routes};
